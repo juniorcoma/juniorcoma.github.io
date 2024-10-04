7bbc043d598be272c6102557c5c3d56c643f70c6
parkhyeonwoo <pq120wal39@gmail.com>
2024-10-05 01:01:10 +0900

    홈페이지 컴포넌트 구현

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,32 @@
-import Canvas from '../components/Canvas';
+import { useState } from 'react';
+import HomeCanvas from '../components/HomeCanvas';
+import HomeTextBox from '../components/HomeTextBox';
+import HomeNextBtn from '../components/HomeNextBtn';
+import styled from 'styled-components';
 
 export default function HomePage() {
+  const [isTextVisible, setIsTextVisible] = useState(false);
   return (
-    <div>
-      <Canvas />
-    </div>
+    <Wrap>
+      <ContentBox>
+        <HomeTextBox isTextVisible={isTextVisible} />
+        <HomeNextBtn isVisible={isTextVisible} />
+      </ContentBox>
+      <HomeCanvas setTextVisible={setIsTextVisible} />
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+`;
+
+const ContentBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 6.4rem 0 12.8rem;
+`;
