34d682bdc54a6da8e7ab11a9d3b55c072f1150ec
parkhyeonwoo <pq120wal39@gmail.com>
2024-10-05 01:06:02 +0900

    ProjectList페이지 구현

---
+++ src/pages/Project.page.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import ProjectList from '../components/ProjectList';
+
+export default function ProjectPage() {
+  return (
+    <Wrap>
+      <ProjectListContainer>
+        <ProjectList type="personal" />
+        <ProjectList type="team" />
+      </ProjectListContainer>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  max-width: 108rem;
+  width: 100%;
+  margin: auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+`;
+
+const ProjectListContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+  width: 100%;
+`;
