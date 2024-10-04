5a91dacabdfd999ceeeaac6333c5721495848f19
parkhyeonwoo <pq120wal39@gmail.com>
2024-10-05 01:04:19 +0900

    TechStack카드 컴포넌트 구현

---
+++ src/components/TechStactCard.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { TECH_STACK_INFO } from '../constant';
+
+interface TechStackCardProps {
+  techStackName: keyof typeof TECH_STACK_INFO;
+}
+
+export default function TechStackCard({ techStackName }: TechStackCardProps) {
+  const { imgSrc, alt, text } = TECH_STACK_INFO[techStackName];
+  return (
+    <TechCard>
+      <TechLogoImg src={imgSrc} alt={alt} />
+      <span>{text}</span>
+    </TechCard>
+  );
+}
+
+const TechCard = styled.div`
+  width: fit-content;
+  min-width: 10rem;
+  padding: 1.2rem;
+  border-radius: 1.6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.8rem;
+  background-color: #31313c;
+  font-size: 1.6rem;
+`;
+const TechLogoImg = styled.img`
+  width: 4rem;
+  aspect-ratio: 1/1;
+`;
