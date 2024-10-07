cbb90459d33e67a4d6e364d9f638186174f93492
parkhyeonwoo <pq120wal39@gmail.com>
2024-10-07 13:08:48 +0900

    outlet을 이용한 고정 nav 구현

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,21 @@ import AboutPage from './pages/About.page';
 import ContactPage from './pages/Contact.page';
 import ProjectPage from './pages/Project.page';
 import ProjectDetailPage from './pages/ProjectDetail.page';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/content" element={<ContentMenuPage />}></Route>
-        <Route path="/about" element={<AboutPage />}></Route>
-        <Route path="/contact" element={<ContactPage />}></Route>
-        <Route path="/project" element={<ProjectPage />}></Route>
-        <Route path="/project/:projectName" element={<ProjectDetailPage />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/content" element={<ContentMenuPage />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+          <Route path="/contact" element={<ContactPage />}></Route>
+          <Route path="/project" element={<ProjectPage />}></Route>
+          <Route path="/project/:projectName" element={<ProjectDetailPage />}></Route>
+        </Route>
       </Routes>
     </>
   );
