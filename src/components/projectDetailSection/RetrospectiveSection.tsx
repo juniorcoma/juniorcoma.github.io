4b8b946c6d4eeef3bbb6ad9446f212cab27bd3fa
parkhyeonwoo <pq120wal39@gmail.com>
2024-10-08 15:48:52 +0900

    프로젝트 페이지 section4 파트 컴포넌트 구현

--- src/components/projectDetailSection/RetrospectiveSection.tsx
+++ src/components/projectDetailSection/RetrospectiveSection.tsx
@@ -1,13 +1,40 @@
 import styled from 'styled-components';
 
-interface RetrospectiveSectionProps {}
+interface RetrospectiveSectionProps {
+  type: 'team' | 'personal';
+  sectionInfo: {
+    link?: string;
+    myFeatureList?: {
+      id: number;
+      feature: string;
+    }[];
+  };
+}
 
-export default function RetrospectiveSection({}: RetrospectiveSectionProps) {
-  return (
-    <Section>
-      <Title>프로젝트 회고록</Title>
-    </Section>
-  );
+export default function RetrospectiveSection({ type, sectionInfo }: RetrospectiveSectionProps) {
+  if (type === 'personal') {
+    return (
+      <Section>
+        <RetrospectiveLink href={sectionInfo.link} target="_blank">
+          <Title>프로젝트 회고록 바로가기</Title>
+        </RetrospectiveLink>
+      </Section>
+    );
+  }
+  if (type === 'team') {
+    return (
+      <Section>
+        <Title>구현한 파트</Title>
+        {sectionInfo.myFeatureList?.map((feature, index) => (
+          <FeatureBox key={feature.id}>
+            <Index>{index + 1}</Index>
+            <FeatureText>{feature.feature}</FeatureText>
+          </FeatureBox>
+        ))}
+      </Section>
+    );
+  }
+  return null;
 }
 
 const Section = styled.section`
@@ -19,6 +46,24 @@ const Title = styled.h2`
   margin-bottom: 3.2rem;
 `;
 
-const RetrospectiveText = styled.p`
+const FeatureText = styled.p`
   font-size: 1.6rem;
+`;
+
+const RetrospectiveLink = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Index = styled.span`
+  font-size: 4.8rem;
+  margin-right: 1.6rem;
+`;
+
+const FeatureBox = styled.div`
+  display: flex;
+  gap: 1.6rem;
+  align-items: center;
+  margin-bottom: 2.4rem;
 `;
